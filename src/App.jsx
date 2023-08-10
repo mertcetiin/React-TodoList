879d8c6d9a7913d39699532c157cb510739f26d3
Mert Çetin <mrtctn506@gmail.com>
2023-08-10 15:20:13 +0300

    ınput.jsx and list.jsx loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Input from './components/Input'
+import List from './components/List'
 
 function App() {
 
 
   return (
     <div>
-
+      <Input />
+      <List />
     </div>
   )
 }
